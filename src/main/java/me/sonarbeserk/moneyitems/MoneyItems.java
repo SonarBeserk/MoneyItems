59d89eb05d9bc9eb64e15875051549904b3d0226
SonarBeserk <sonarbeserk@gmail.com>
2014-03-16 23:16:16 -0400

    update id message and actually update

--- src/main/java/me/sonarbeserk/moneyitems/MoneyItems.java
+++ src/main/java/me/sonarbeserk/moneyitems/MoneyItems.java
@@ -105,6 +105,8 @@ public class MoneyItems extends JavaPlugin {
         new MoneyAPI(this);
 
         getServer().getPluginManager().registerEvents(new ItemPickupListener(this), this);
+
+        checkForUpdates();
     }
 
     private boolean setupEconomy() {
@@ -125,7 +127,7 @@ public class MoneyItems extends JavaPlugin {
 
             if(getConfig().getString("settings.updater.mode").equalsIgnoreCase("notify")) {
 
-                updater = new Updater(this, /*replace with the proper id*/, getFile(), Updater.UpdateType.NO_DOWNLOAD, getConfig().getBoolean("settings.updater.notify-downloads"));
+                updater = new Updater(this, /*replace with the proper id when distributing*/, getFile(), Updater.UpdateType.NO_DOWNLOAD, getConfig().getBoolean("settings.updater.notify-downloads"));
 
                 if(Double.parseDouble(getDescription().getVersion().replaceAll("[a-zA-Z]", "")) == Double.parseDouble(this.updater.getLatestName().replaceAll("[a-zA-Z]", ""))) {
 
@@ -139,7 +141,7 @@ public class MoneyItems extends JavaPlugin {
                 }
             } else if(getConfig().getString("settings.updater.mode").equalsIgnoreCase("update")) {
 
-                updater = new Updater(this, /*replace with the proper id*/, getFile(), Updater.UpdateType.DEFAULT, getConfig().getBoolean("settings.updater.notify-downloads"));
+                updater = new Updater(this, /*replace with the proper id when distributing*/, getFile(), Updater.UpdateType.DEFAULT, getConfig().getBoolean("settings.updater.notify-downloads"));
 
                 if(Double.parseDouble(getDescription().getVersion().replaceAll("[a-zA-Z]", "")) == Double.parseDouble(updater.getLatestName().replaceAll("[a-zA-Z]", ""))) {
 
