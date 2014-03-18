0baf35e49c5bcb1ad4dd06849a534fb196d8d07b
SonarBeserk <sonarbeserk@gmail.com>
2014-03-18 17:19:46 -0400

    fix typo

--- src/main/java/me/sonarbeserk/moneyitems/MoneyAPI.java
+++ src/main/java/me/sonarbeserk/moneyitems/MoneyAPI.java
@@ -53,7 +53,7 @@ public class MoneyAPI {
      * Spawns an amount of money at a location
      * @param location the location to spawn the money it
      * @param material the material to use for the money
-     * @param stackSize to size of the itemstack to spawn (used for visual purposes)
+     * @param stackSize the size of the itemstack to spawn (used for visual purposes)
      * @param worth the total worth of the money spawned
      */
     public void spawnMoney(Location location, Material material, int stackSize, int worth) {
